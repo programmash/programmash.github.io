2ac083d7f0162bd53ba6d9c1ef20400c502bcc79
programmash <104310521+programmash@users.noreply.github.com>
2022-07-07 13:49:21 +0530

    rect area file

---
+++ rectarea.js
@@ -0,0 +1,7 @@
+function areaofrectangle(){
+	
+   var length = document.getElementById("side1").value;
+   var bredth = document.getElementById("side2").value;
+   var output = length*bredth;
+   console.log(output);
+}
