585f7178c94b0b8b93f70b92b8477dd41c9f5d9f
programmash <104310521+programmash@users.noreply.github.com>
2022-07-07 13:56:44 +0530

    Add files via upload

---
+++ areacir.js
@@ -0,0 +1,11 @@
+ function areaofcircle() {
+            var radius = document.getElementById('txtRadius1').value;
+            alert("The area of the circle is " + (radius * radius * Math.PI));
+		    
+        }
+		
+function perofcircle(){
+	 var radius = document.getElementById('txtRadius2').value;
+      alert("The perimeter of the circle is " + (2* radius * Math.PI));
+	
+}		
