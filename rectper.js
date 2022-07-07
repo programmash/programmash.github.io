8f27e23f42755979322212c478a20a3ab0af3020
programmash <104310521+programmash@users.noreply.github.com>
2022-07-07 13:51:12 +0530

    Add files via upload

---
+++ rectper.js
@@ -0,0 +1,7 @@
+function perofrectangle(){
+	
+   var length = document.getElementById("side1").value;
+   var bredth = document.getElementById("side2").value;
+   var output = 2*side1 + 2*side2;
+   console.log(output);
+}
