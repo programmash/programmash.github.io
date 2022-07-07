e2777dd37a53acc72fde01959da3d9920f31c0b6
programmash <104310521+programmash@users.noreply.github.com>
2022-07-07 20:37:13 +0530

    calculator file

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function add(){
+	const num1= document.getElementById('num1').value;
+	const num2= document.getElementById('num1').value;
+	const sum= parseInt(num1)+ parseInt (num2);	
+	console.log(sum);
+}
+function sub(){
+	const num1= document.getElementById('num1').value;
+	const num2= document.getElementById('num1').value;
+	const sub= parseInt(num1) - parseInt (num2);
+    console.log(sub);	
+}
+function multiplication(){
+	const num1= document.getElementById('num1').value;
+	const num2= document.getElementById('num1').value;
+	const mul= parseInt(num1)* parseInt (num2);	
+	console.log(mul);
+}
+function div(){
+	const num1= document.getElementById('num1').value;
+	const num2= document.getElementById('num1').value;
+	const div= parseInt(num1)/ parseInt (num2);	
+	console.log(div);
+}
